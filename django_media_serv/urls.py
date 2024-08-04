8b324f757e7499a651f708041370c9d394e90ba8
Jason Moore <jason.moore@dbca.wa.gov.au>
2024-08-04 14:13:37 +0800

    Django 4 and 5 support with backwards compatibility

--- django_media_serv/urls.py
+++ django_media_serv/urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls import url, include
+from django import http, VERSION
+
+
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django_media_serv import views
@@ -6,6 +8,19 @@ from django.contrib.auth import logout, login
 from django.contrib.auth.views import LogoutView, LoginView
 from django.conf import settings
 
-urlpatterns = [
+
+if VERSION < (3, 0):
+    from django.conf.urls import url, include
+    urlpatterns = [
         url(r'^media/', views.getMediaFile, name='getMediaFile'),
-]
+    ]
+
+else:
+    from django.urls import re_path
+    urlpatterns = [
+            re_path(r'^media/', views.getMediaFile, name='getMediaFile'),
+    ]
+
+
+
+
